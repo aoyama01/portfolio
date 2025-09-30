95a04026dbda5c6d817bf1459f5e30c769afb54b
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-09-30 15:09:07 +0900

    config: install ndx, zod and etc.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+import rehypeHighlight from "rehype-highlight";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeHighlight, rehypeSlug],
+  },
+});
+
+export default withMDX(nextConfig);
