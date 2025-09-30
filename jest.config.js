221f3586579eeee097437863e184a57f0eef2f6e
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2025-09-30 14:32:53 +0900

    config: prettier, jest and husky

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+// jest.config.mjs もしくは jest.config.ts
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.stories.{js,jsx,ts,tsx}",
+    "!src/**/__tests__/**",
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
+
+export default createJestConfig(customJestConfig);
